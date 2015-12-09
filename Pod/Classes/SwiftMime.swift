a78b9fd55590fb27cd1fcfdbbb56a75235ce7977
Daniel Seither <post@tiwoc.de>
2015-12-09 16:29:22 +0100

    Upgrade to Xcode 7.2

--- Pod/Classes/SwiftMime.swift
+++ Pod/Classes/SwiftMime.swift
@@ -26,20 +26,20 @@ public class SwiftMime{
     public func define(map: NSDictionary){
         
         for type in map{
-            var exts: NSArray = type.value as NSArray
+            let exts: NSArray = type.value as! NSArray
             if(exts.count == 0){
             }
             for var index = 0;index < exts.count; ++index {
-                types[exts[index] as NSString] = type.key as? NSString
+                types[exts[index] as! NSString] = type.key as? NSString
             }
             
-            extensions[type.key as NSString] = exts[0] as? NSString;
+            extensions[type.key as! NSString] = exts[0] as? NSString;
         }
     }
     
     func loadTypesFile(filePath: String){
         let path =  NSBundle(forClass: object_getClass(self)).pathForResource(filePath, ofType: "types")
-        var possibleContent = NSString(contentsOfFile:path!, encoding: NSUTF8StringEncoding, error: nil)
+        let possibleContent = try? NSString(contentsOfFile:path!, encoding: NSUTF8StringEncoding)
         var lines = NSArray()
         let map = NSMutableDictionary()
         if let content = possibleContent {
@@ -49,9 +49,9 @@ public class SwiftMime{
             if line.hasPrefix("#"){
                 continue
             }
-            var fields: NSArray = line.stringByReplacingOccurrencesOfString("\\s+", withString:" ", options: .RegularExpressionSearch, range: NSMakeRange(0, line.length)).componentsSeparatedByString(" ")
+            let fields: NSArray = line.stringByReplacingOccurrencesOfString("\\s+", withString:" ", options: .RegularExpressionSearch, range: NSMakeRange(0, line.length)).componentsSeparatedByString(" ")
             if fields.firstObject?.length>0 {
-                map[fields.firstObject as NSString] = fields.subarrayWithRange(NSMakeRange(1, fields.count-1))
+                map[fields.firstObject as! NSString] = fields.subarrayWithRange(NSMakeRange(1, fields.count-1))
             }
         }
         define(map)
@@ -59,7 +59,7 @@ public class SwiftMime{
     
     public func lookupType(path: NSString) -> NSString!{
         let newPath = path.stringByReplacingOccurrencesOfString(".*[\\.\\/\\\\]", withString: "", options: .RegularExpressionSearch, range: NSMakeRange(0, path.length))
-        var ext: NSString = newPath.lowercaseString
+        let ext: NSString = newPath.lowercaseString
         return types[ext]!
     }
     
