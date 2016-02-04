2c3c25a7f9ae8a4a931e283238d5ba74684b69ed
di wu <di.wu@me.com>
2016-02-04 15:26:52 +0800

    add pods files to support carthage

---
+++ Example/Pods/Target Support Files/SwiftMime/SwiftMime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftMime : NSObject
+@end
+@implementation PodsDummy_SwiftMime
+@end
---
+++ Example/Pods/Target Support Files/SwiftMime/SwiftMime-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SwiftMimeVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftMimeVersionString[];
+
